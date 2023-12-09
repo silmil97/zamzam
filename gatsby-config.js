a6a70612a8b5844452e10095aebfe56a3428d427
silmil97 <ahmadabbaiibo@googlemail.com>
2023-12-09 12:12:24 +0100

    feat: add env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,22 @@
+require('dotenv').config();
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
-    title: `zamzamContentful`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'zamzamContentful',
+    siteUrl: 'https://www.yourdomain.tld',
   },
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "TAE6Or3gG2IL1I1D9HAUqMT5_DENjbxGRHWY054SzUM",
-      "spaceId": "jqq7u9aozdtr"
-    }
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+    },
   },
   'gatsby-plugin-image',
-  `gatsby-plugin-sharp`,
+  'gatsby-plugin-sharp',
   'gatsby-plugin-postcss',
-  
-  ]
+  ],
 };
